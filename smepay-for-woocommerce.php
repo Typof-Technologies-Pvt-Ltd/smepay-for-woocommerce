e565f8e6fdf48fbcf4a78b57f1811830fb4c5312
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2025-06-06 12:08:07 +0530

    changed class name

--- smepay-for-woocommerce.php
+++ smepay-for-woocommerce.php
@@ -1,66 +1,62 @@
 <?php
 /**
  * Plugin Name: SMEPay for WooCommerce
- * Plugin URI: https://smepay.io/
+ * Plugin URI:  https://smepay.io/
  * Description: Accept UPI payments via SMEPay with QR code widget on your WooCommerce website.
- * Version: 1.0.0
- *
- * Author: SMEPay
- * Author URI: https://smepay.io/
- *
+ * Version:     1.0.0
+ * Author:      SMEPay
+ * Author URI:  https://smepay.io/
  * Text Domain: smepay-for-woocommerce
  * Domain Path: /languages/
+ * Requires Plugins: woocommerce
  *
  * Requires at least: 4.7
- * Tested up to: 6.8
+ * Tested up to:      6.8
  *
- * Copyright: © Typof.
- * License: GNU General Public License v3.0
+ * Copyright:  © Typof.
+ * License:    GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 // Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
-defined('ABSPATH') || exit;
+// Define plugin version constant.
+define( 'SMEPAY_WC_VERSION', '1.0.0' );
 
-// Define plugin version constant
-define('SMEPAY_WC_VERSION', '1.0.0');
+// Define absolute server path to the plugin directory.
+define( 'SMEPAY_WC_PATH', plugin_dir_path( __FILE__ ) );
 
-// Define absolute server path to the plugin directory
-define('SMEPAY_WC_PATH', plugin_dir_path(__FILE__));
-
-// Define URL to the plugin directory (for loading assets)
-define('SMEPAY_WC_URL', plugin_dir_url(__FILE__));
-
+// Define URL to the plugin directory (for loading assets).
+define( 'SMEPAY_WC_URL', plugin_dir_url( __FILE__ ) );
 
 /**
- * WC Dummy Payment gateway plugin class.
+ * Main plugin class for SMEPay WooCommerce integration.
  *
- * @class WC_SMEPay_Payments
+ * @package Smepay_For_WooCommerce
  */
-class WC_SMEPay_Payments {
+class Smepay_For_WooCommerce {
 
     /**
      * Plugin bootstrapping.
      */
     public static function init() {
 
-        // Dummy Payments gateway class.
+        // SMEPay gateway class.
         add_action( 'plugins_loaded', array( __CLASS__, 'includes' ), 0 );
 
-        // Make the Dummy Payments gateway available to WC.
+        // Make the SMEPay Payments gateway available to WC.
         add_filter( 'woocommerce_payment_gateways', array( __CLASS__, 'add_gateway' ) );
-        
 
         // Registers WooCommerce Blocks integration.
         add_action( 'woocommerce_blocks_loaded', array( __CLASS__, 'woocommerce_gateway_smepay_woocommerce_block_support' ) );
-
     }
 
     /**
-     * Add the Dummy Payment gateway to the list of available gateways.
+     * Add the SMEPay Payment gateway to the list of available gateways.
      *
-     * @param array
+     * @param array $gateways Array of existing gateways.
+     * @return array Modified array of gateways including SMEPay.
      */
     public static function add_gateway( $gateways ) {
 
@@ -75,6 +71,7 @@ class WC_SMEPay_Payments {
         if ( ( 'yes' === $hide_for_non_admin_users && current_user_can( 'manage_options' ) ) || 'no' === $hide_for_non_admin_users ) {
             $gateways[] = 'WC_Gateway_SMEPay';
         }
+
         return $gateways;
     }
 
@@ -83,27 +80,27 @@ class WC_SMEPay_Payments {
      */
     public static function includes() {
 
-        require_once 'includes/class-smepay-utils.php';
+        require_once SMEPAY_WC_PATH . 'includes/class-smepay-utils.php';
 
-        // Make the WC_Gateway_Dummy class available.
+        // Make the SMEPay gateway available to WooCommerce.
         if ( class_exists( 'WC_Payment_Gateway' ) ) {
-            require_once 'includes/class-wc-gateway-smepay.php';
+            require_once SMEPAY_WC_PATH . 'includes/class-wc-gateway-smepay.php';
         }
     }
 
     /**
-     * Plugin url.
+     * Get plugin URL.
      *
-     * @return string
+     * @return string Plugin URL.
      */
     public static function plugin_url() {
         return untrailingslashit( plugins_url( '/', __FILE__ ) );
     }
 
     /**
-     * Plugin url.
+     * Get plugin absolute path.
      *
-     * @return string
+     * @return string Plugin absolute path.
      */
     public static function plugin_abspath() {
         return trailingslashit( plugin_dir_path( __FILE__ ) );
@@ -111,11 +108,13 @@ class WC_SMEPay_Payments {
 
     /**
      * Registers WooCommerce Blocks integration.
-     *
      */
     public static function woocommerce_gateway_smepay_woocommerce_block_support() {
+
         if ( class_exists( 'Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType' ) ) {
-            require_once 'includes/blocks/class-wc-smepay-payments-blocks.php';
+
+            require_once SMEPAY_WC_PATH . 'includes/blocks/class-wc-smepay-payments-blocks.php';
+
             add_action(
                 'woocommerce_blocks_payment_method_type_registration',
                 function( Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry ) {
@@ -126,4 +125,7 @@ class WC_SMEPay_Payments {
     }
 }
 
-WC_SMEPay_Payments::init();
+// Initialize plugin only if WooCommerce is active.
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+    Smepay_For_WooCommerce::init();
+}
